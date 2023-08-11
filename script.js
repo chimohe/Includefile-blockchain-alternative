c20bb0a3fbc590e7f3ac45a891eef7b5925814f4
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2023-08-11 07:47:54 -0400

    script.js
    In this example, the JavaScript code dynamically adds blocks to the form when the “Add Block” button is clicked. It then gathers the form data and sends it to the process_form.php script using the fetch API when the “Submit” button is clicked.
    
    Remember that this is a simplified example, and you should incorporate proper validation and security measures in a real-world application. Additionally, the process_form.php script will need to handle the submitted data and files as in the previous examples.

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const addDataBtn = document.getElementById("addDataBtn");
-    const outputDiv = document.getElementById("output");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blockchain Data Entry</title>
+</head>
+<body>
+    <h1>Blockchain Data Entry</h1>
+    <form id="blockchain-form">
+        <!-- Dynamic block creation using JavaScript -->
+    </form>
+    <button id="add-block">Add Block</button>
+    <button id="submit-form">Submit</button>
 
-    addDataBtn.addEventListener("click", async () => {
-        try {
-            const response = await fetch("add_data.php"); // Change to the actual endpoint
-            const result = await response.text();
-            outputDiv.innerHTML = result;
-        } catch (error) {
-            console.error("Error adding data:", error);
-            outputDiv.innerHTML = "Error adding data to the blockchain file.";
-        }
-    });
-});
+    <script>
+        const form = document.getElementById('blockchain-form');
+        const addBlockButton = document.getElementById('add-block');
+        const submitButton = document.getElementById('submit-form');
+        let blockCount = 0;
+
+        addBlockButton.addEventListener('click', () => {
+            blockCount++;
+            const blockDiv = document.createElement('div');
+            blockDiv.innerHTML = `
+                <h2>Block ${blockCount}</h2>
+                <textarea name="block${blockCount}_data" rows="4" cols="50" placeholder="Enter Block ${blockCount} data"></textarea>
+                <br>
+                <input type="file" name="file_block${blockCount}">
+                <br>`;
+            form.insertBefore(blockDiv, addBlockButton);
+        });
+
+        submitButton.addEventListener('click', async () => {
+            const formData = new FormData(form);
+            try {
+                const response = await fetch('process_form.php', {
+                    method: 'POST',
+                    body: formData
+                });
+                if (response.ok) {
+                    alert('Data submitted successfully');
+                } else {
+                    alert('Error submitting data');
+                }
+            } catch (error) {
+                console.error('An error occurred:', error);
+            }
+        });
+    </script>
+</body>
+</html>
